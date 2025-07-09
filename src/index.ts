b8f4ff378708d0254ad3c0bb2a08233ca0480d76
Thomas Sauvage <144670432+cactusninjaa@users.noreply.github.com>
2025-07-09 14:34:52 +0200

    expose currency on index

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './converters/length';
 export * from './converters/weight';
 export * from './converters/temperature';
 export * from './converters/volume';
+export * from './converters/currency'
