2aaa07bba86dd4ba24e8a17ba4529cd28f406bb5
Anthony Lopes <144701824+AnthxnyL@users.noreply.github.com>
2025-07-09 16:04:37 +0200

    ADD ci operation (#11)
    * ADD ci operation
    
    * ADD ci operation
    
    ---------
    
    Co-authored-by: Anthony <anthonylopes037@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,46 @@
 import js from "@eslint/js";
 import globals from "globals";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettier from "eslint-config-prettier";
+
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-]);
+  // JS
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: globals.node,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    plugins: { js },
+    rules: {
+      ...js.configs.recommended.rules,
+    },
+  },
 
+  // TS
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: "./tsconfig.json",
+      },
+      globals: globals.node,
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+    },
+  },
+
+  // Prettier (optional, disables conflicting rules)
+  prettier,
+]);
