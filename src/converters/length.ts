fb76dccaec0fe61cc7498010e6529c3c569e43b8
Anthony <anthonylopes037@gmail.com>
2025-07-09 11:55:00 +0200

    ADD converters functions

--- src/converters/length.ts
+++ src/converters/length.ts
@@ -1,3 +1,56 @@
+// Meters and Feet Conversions
 export function metersToFeet(meters: number): number {
   return meters * 3.28084;
 }
+
+export function feetToMeters(feet: number): number {
+  return feet / 3.28084;
+}
+
+
+// Kilometers and Miles Conversions
+export function kilometersToMiles(kilometers: number): number {
+  return kilometers * 0.621371;
+}
+
+export function milesToKilometers(miles: number): number {
+  return miles / 0.621371;
+}
+
+// Yards and Meters Conversions
+export function yardsToMeters(yards: number): number {
+  return yards * 0.9144;
+}
+
+export function metersToYards(meters: number): number {
+  return meters / 0.9144;
+}
+
+// Yards and Feet Conversions
+export function yardsToFeet(yards: number): number {
+  return yards * 3;
+}
+
+export function feetToYards(feet: number): number {
+  return feet / 3;
+}
+
+// Kilometers and Yards Conversions
+export function kilometersToYards(kilometers: number): number {
+  return kilometers * 1093.61;
+} 
+
+export function yardsToKilometers(yards: number): number {
+  return yards / 1093.61;
+}
+
+// Kilometers and Feet Conversions
+export function kilometersToFeet(kilometers: number): number {
+  return kilometers * 3280.84;
+} 
+
+export function feetToKilometers(feet: number): number {
+  return feet / 3280.84;
+}
+
+
